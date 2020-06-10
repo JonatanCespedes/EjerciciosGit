3244d450c048cebfe0385bc626b614e4b9429237
JonatanCespedes <retratos.jonatan@gmail.com>
2020-06-10 17:20:10 -0300

    Bateristas es la que va!

--- array.js
+++ array.js
@@ -1,2 +1,3 @@
 let musicos = ["Charly García", "Luis Alberto Spinetta", "Gustavo Cerati","Mike Portnoy"];
 let guitarristas = ["Slash", "Brian Setzer", "Tony Iommi"]
+let bateristas = ["Lars Ulrich", "Tomas Moro", "Ringo Star"];
