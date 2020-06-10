0ffdab9b37beb7d325330ca0afac5bf3d475da2b
JeremiasCastillo25 <jerecastillo25@gmail.com>
2020-06-10 17:28:17 -0300

    array nuevo

--- array.js
+++ array.js
@@ -1,7 +1,4 @@
 let musicos = ["Charly García", "Luis Alberto Spinetta", "Gustavo Cerati","Mike Portnoy"];
-<<<<<<< HEAD
 let guitarristas = ["Slash", "Brian Setzer", "Tony Iommi"]
 let bateristas = ["Lars Ulrich", "Tomas Moro", "Ringo Star"];
-=======
 let guitarristas = ["Slash", "Brian Setzer", "Tony Iommi","Pepe grillo"]
->>>>>>> c1942ffc21c08b26db308fa826f0633feddfeac9
